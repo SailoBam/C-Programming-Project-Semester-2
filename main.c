3890a6e172f974e606bbd3c9045d57d1c039a803
SailoBam <francisbamsey@gmail.com>
2026-02-03 17:16:23 +0000

    implemented sign in to main

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include "sqlite3.c"
 #include "sqlite3.h"
 #include "SQLServiceLayer.c"
+#include "loginSignUp.c"
 
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
@@ -10,5 +11,6 @@
 int main(int argc, char *argv[]) {
 	
 	OpenDB();
+	startlogin();
 	return 0;
 }
